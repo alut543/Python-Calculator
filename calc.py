28e6001df3716889fb19782e508a28d8f5a33738
alut543 <aluta.r.231@isg.edu.sa>
2025-05-15 17:18:06 +0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,35 +1,28 @@
 import time
-import random
 
-: : Put this python file in VSCode or a IDLE Shell or other third-party software that will allow you
-: : to use this.
-
-print("Calculator ➖✖️➕➗")
-print(" ")
-print(" ")
-print("What mode would you like to choose?")
-cmdline = print("> ")
-if cmdline == "addition":
-    time.sleep(3)
-    firstnumadd = int(print("Enter the first number. > "))
-    secnumadd = int(print("Enter the second number. > "))
-    addition2 = int(print(firstnumadd + secnumadd))
-    else
-    if cmdline == "subtraction":
-        firstnumsub = int(print("Enter the first number. > "))
-        secnumsub = int(print("Enter the second number. > "))
-        subtraction2 = int(print(firstnumsb - secnumsub))
-        else
-        if cmdline == "multiplication"
-          firstnummult = int(print("Enter the first number. > "))
-          secnummult = int(print("Enter the second number. > " ))
-          multiply = int(print(firstnummult * secnummult. > " ))
-           else
-          if cmdline == "division":
-                               firstnumdiv = int(print("Enter the first number. > "))
-                              secnumdiv = int(print("Enter the second number. > "))
-                              divide = int(print(firstnumdiv / secnumdiv)
-
+while True:
+print("Calculator ➖✖️➕➗:")
+    cmdline = input("> ")
+    if cmdline == "addition":
+        setnumaddfirst = int(input("Enter your first number. > "))
+        setnumaddsecond = int(input("Enter your second number. > "))
+        time.sleep(2)
+        print(setnumaddfirst + setnumaddsecond)
+    elif cmdline == "subtraction":
+            setnumsubfirst = int(input("Enter your first number. > "))
+            setnumsubsecond = int(input("Enter your second number. > "))
+            time.sleep(2) 
+            print(setnumsubfirst - setnumaddsecond)
+    elif cmdline == "multiplication":
+            setnummultfirst = int(input("Enter your first number. > "))
+            setnummultsecond = int(input("Enter your second number"))
+            time.sleep(2)
+            print(setnummultfirst * setnummultsecond)
+    elif cmdline == "division":
+           setnumdivfirst = int(input("Enter your first number. > "))
+           setnumdivsecond = int(input("Enter your second number. > "))
+           time.sleep(2)
+           print(setnumdivfirst + setnumdivsecond)
                                             
 
 
